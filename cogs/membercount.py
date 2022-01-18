30373d4dc38aa91367e019e0d14c4dcfff36d6ec
ROBERTGUO19 <44046531+ROBERTGUO19@users.noreply.github.com>
2022-01-18 11:15:08 -0500

    updated

--- cogs/membercount.py
+++ cogs/membercount.py
@@ -4,14 +4,21 @@ import discord.utils
 
 
 class Members(commands.Cog):
-    def __init__(self, bot):
+    def init(self, bot):
         self.bot = bot
-    
-    @commands.command(name='membercount')
-    async def members(self, ctx, member):
-        guild = member.guild
-        await ctx.send(f'test{len(guild.member_count)}')
 
+    @commands.command(name='membercount')
+    async def members(self, ctx):
+        member_count = 0
+        for member in ctx.guild.members:
+            member_count += 1
+        await ctx.send(member_count)
+    #    print(ctx.guild.member_count)
+    #    await ctx.send(f'test{len(ctx.guild.member_count)}')
+    # @commands.command(name='membercount')
+    # async def get_guild_member_count(guild, self, ctx):
+    #     """ returns the member count of a guild """
+    #     ctx.send(len(guild.members))
 
 
 
