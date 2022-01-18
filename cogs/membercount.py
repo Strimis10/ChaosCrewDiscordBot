62cca62e96a0cacb7afaac6f69a5f090e554d9f7
ROBERTGUO19 <44046531+ROBERTGUO19@users.noreply.github.com>
2022-01-18 12:28:05 -0500

    fixed

--- cogs/membercount.py
+++ cogs/membercount.py
@@ -9,18 +9,7 @@ class Members(commands.Cog):
 
     @commands.command(name='membercount')
     async def members(self, ctx):
-        member_count = 0
-        for member in ctx.guild.members:
-            member_count += 1
-        await ctx.send(member_count)
-    #    print(ctx.guild.member_count)
-    #    await ctx.send(f'test{len(ctx.guild.member_count)}')
-    # @commands.command(name='membercount')
-    # async def get_guild_member_count(guild, self, ctx):
-    #     """ returns the member count of a guild """
-    #     ctx.send(len(guild.members))
-
-
+        await ctx.send(ctx.guild.member_count)
 
 def setup(bot):
     bot.add_cog(Members(bot))
