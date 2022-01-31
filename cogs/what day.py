f87d5e6d6366093e9a9f5c83e0d6f73d86e66b20
Strimis10 <Strimis10@gmail.com>
2022-01-31 07:52:11 +0100

    birthday thing

--- cogs/what day.py
+++ cogs/what day.py
@@ -1,36 +1,63 @@
 #pip install schedule
 
-from datetime import date
+
 import time
-from discord.ext import commands
+from discord.ext import commands, tasks
 import discord
 import discord.utils
 import json
 
-list = {}
 
-current = date.today()
 
-class fun(commands.Cog):
+class birthday(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.index = 0
+        self.printer.start()
 
-    # @commands.command(name='say',description='bot says [text]',brief='bot says [text]')
-    # async def say(self, ctx, *, text: commands.clean_content = ''):
-    #     #                         ^^ This is for pings / mentions being cleaned so you can't do `a!say @everyone`.
-    #     #command content
-    
-    date = date.today()
-    # while current == date.today():
-    with open("birthdays.json") as f:       
-        data = json.load(f) 
-        for user in data:
-            print(user)
-            for number in range(4, 9):
-                if data[user[number]] == date[number]:
-                    print("ugh")
-            print(data[user])
-    #     print(date)
+    def birthday_unload(self):
+        self.printer.cancel()
+
+    @tasks.loop(seconds=5.0)
+    async def printer(self):
+        print(self.index)
+        self.index += 1
+
+        from datetime import date
+        list = {}
+
+        current = date.today()
+
+        datel = []
+        date = str(date.today())
+        for number in date:
+            datel.append(number)
+        # while current == date.today():
+        with open("birthdays.json") as f:       
+            data = json.load(f) 
+            for user in data:
+                print(user)
+                day = []
+                for number in data[user]:
+                    day.append(number)
+                match = 0
+                for number in range(4, 9):
+                    if day[number] == datel[number]:
+                        match += 1
+                        print("ugh")
+
+                if match == 5:
+                    # await ctx.send("Happy")
+                    await commands.send("Happy")
+                    print("Happy birthday!!")
+        #     print(date)
+       
+       
+       
+        # @printer.before_loop
+        # async def before_printer(self):
+        #     print('waiting...')
+        #     await self.bot.wait_until_ready()
            
         
         
@@ -40,6 +67,6 @@ class fun(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(fun(bot))
+    bot.add_cog(birthday(bot))
 
 
