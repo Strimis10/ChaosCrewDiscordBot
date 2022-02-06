bdea81f0eae1f0988504173d1a068a0e62c54f30
Strimis10 <Strimis10@gmail.com>
2022-02-06 11:34:57 +0100

    fgyhnvf

--- cogs/baned_words.py
+++ cogs/baned_words.py
@@ -7,15 +7,32 @@ import json
 import os
 a = []
 e = {}
+from typing import Optional
 import random
+import time
+import datetime
+import humanfriendly
+import nextcord
+
+
+word_immune = []
 
 
 def owner_admin_or_roles(ctx, user: discord.Member):
     roles = [786014220721979445, 786014064533831690, 933127964248375337, "", "Developers", "Admin"]
+    with open("word_immune.json") as feedsjson: 
+        feeds = json.load(feedsjson)
 
+    print(feeds)
+    print("sdd")
+        
+    ctx.send(ctx.message.author.id)
 
     if ctx.message.author.id in [386826952599928842, 427822985102098434]:
         return True
+
+    elif ctx.message.author.id in feeds:
+        return True 
 
     elif "786014220721979445" in [y.id for y in ctx.message.author.roles]:
         return True
@@ -57,6 +74,8 @@ def owner_admin_or_roles(ctx, user: discord.Member):
 def owner_admin_or_roles_message(message, user: discord.Member):
     roles = [786014220721979445, 786014064533831690, 933127964248375337, "Developers", "Admin"]
 
+    
+
 
     if message.author.id in [386826952599928842, 427822985102098434]:
         return True
@@ -79,6 +98,10 @@ def owner_admin_or_roles_message(message, user: discord.Member):
     elif "Developers" in [y.name.lower() for y in message.author.roles]:
         return True
 
+    elif "Owner" in [y.name.lower() for y in message.author.roles]:
+        return True
+    
+    
     # elif role2 in user.roles:
     #     return True
 
@@ -216,23 +239,44 @@ class usefull(commands.Cog):
                 if rand2 == 1:
                     await message.reply("EEVEE: Dead")
 
+    @commands.has_permissions(administrator=True)
+    @commands.command(name='word_immunity',aliases=["WI", "wi", "wordimmunity"],description='Bot grants the user imunity from the Banned_words function and everything it does.')
+    async def word_immunity(self, ctx, target: Optional[discord.Member]):
+        target = target or ctx.author
+        
+
+        
+        if not os.path.isfile("word_immune.json"):
+            word_immune.append(target.id)
+            
+            with open("word_immune.json", mode='w') as f:
+                f.write(json.dumps(word_immune, indent=2))
+
+        else: 
+            with open("word_immune.json") as feedsjson: 
+                feeds = json.load(feedsjson)
+
+            feeds.append(target.id)
+        
+            with open("word_immune.json", mode='w') as f:
+                f.write(json.dumps(feeds, indent=2))
+
 
     @commands.Cog.listener()
-    async def on_message(self, message,):
+    async def on_message(self, message):
         if owner_admin_or_roles_message(message= message, user=discord.Member) == False:
             if message.author.id != 932687176997687316:
                 with open("banned_words.json") as oj: 
                         o = json.load(oj)
                         for word in o:
                             if word in message.content.lower():
-                                await message.reply(f"{message.author.mention} That's a Banned word, this will be logged and after multible incidents you will be timed out, kicked and then banned. use '?bws' to se a list of the banned words")
-                                await message.delete()
 
                                 if not os.path.isfile("incidents.json"):
                                     e[str(message.author.id)] = 1
                                     
                                     with open("incidents.json", mode='w') as f:
                                         f.write(json.dumps(e, indent=2))
+                                
                                        
                                 else: 
                                     with open("incidents.json") as feedsjson: 
@@ -251,6 +295,32 @@ class usefull(commands.Cog):
                                             with open("incidents.json", mode='w') as f:
                                                 f.write(json.dumps(feeds, indent=2)) 
                                     
+                                    with open("incidents.json") as feedsjson: 
+                                        feeds = json.load(feedsjson)
+
+                                    if feeds[str(message.author.id)] == 1:
+                                        await message.reply(f"{message.author.mention} That's a Banned word, this is your 1:st incident, this will be logged and after 4 incidents you will be timed out, 5: kicked and 6: then banned. use '?bws' to se a list of the banned words")
+                                        await message.delete()
+
+                                    elif feeds[str(message.author.id)] == 2:
+                                        await message.reply(f"{message.author.mention} That's a Banned word, this is your 2:nd incident, this will be logged and after 4 incidents you will be timed out, 5: kicked and 6: then banned. use '?bws' to se a list of the banned words")
+                                        await message.delete()
+
+                                    elif feeds[str(message.author.id)] == 3:
+                                        await message.reply(f"{message.author.mention} That's a Banned word, this is your 3:d incident, this will be logged and after 4 incidents you will be timed out, 5: kicked and 6: then banned. use '?bws' to se a list of the banned words")
+                                        await message.delete()
+
+                                    elif feeds[str(message.author.id)] == 4:
+                                        await message.reply(f"{message.author.mention} That's a Banned word, this is your 4:th incident, you will be timed out for 24 hours")
+                                        await message.delete()
+                                        time.sleep(15)
+                                        await message.author.edit(timeout=nextcord.utils.utcnow()+86400)
+                                        print("timed")
+                                    await message.delete()
+
+
+                                    
+                                        
 
 
                                     # try:
