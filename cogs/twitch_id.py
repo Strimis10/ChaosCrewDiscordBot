90514d06dbc4207688c3d3c8a770d3f4cf64a117
Strimis10 <Strimis10@gmail.com>
2022-02-09 13:01:06 +0100

    Update twitch_id.py

--- cogs/twitch_id.py
+++ cogs/twitch_id.py
@@ -3,7 +3,7 @@ import discord
 import discord.utils
 import requests
 import json
-import Twitch
+
 
 class id(commands.Cog):
     def __init__(self, bot):
@@ -11,41 +11,42 @@ class id(commands.Cog):
 
     @commands.command(name='id',description='bot says [text]',brief='bot says [text]')
     async def say(self, ctx, *, text: commands.clean_content = ''):
-        print(requests.get("https://api.twitch.tv/helix/users?login=<Kennevo>"))
-
-        #if text == '':
-            #ctx.send("you need to send your Twitch username")
         
-        #else:
+        client_id = ''
+        client_secret = ''
+        streamer_name = 'Kennevo'
 
-#            client_id = id
-#            client_secret = '(secret)'
-#            streamer_name = 'sasdaasdsdasad'
-#
-#            body = {
-#                'client_id': client_id,
-#                'client_secret': client_secret,
-#                "grant_type": 'client_credentials'
-#            }
-#            r = requests.post('https://id.twitch.tv/oauth2/token', body)
-#
-#            #data output
-#            keys = r.json()
-#
-#            print(keys)
-#
-#            headers = {
-#                'Client-ID': client_id,
-#                'Authorization': 'Bearer ' + keys['access_token']
-#            }
-#            r= requests.get("https://api.twitch.tv/helix/streams?user_login=kitboga",headers=headers)
-#            q=json.loads(r.text)
-#
-#            print(q)
-#
+        body = {
+            'client_id': client_id,
+            'client_secret': client_secret,
+            "grant_type": 'client_credentials'
+        }
+        r = requests.post('https://id.twitch.tv/oauth2/token', body)
 
-        #                         ^^ This is for pings / mentions being cleaned so you can't do `a!say @everyone`.
-        #command content
+        #data output
+        keys = r.json()
+
+        print(keys)
+
+        headers = {
+            'Client-ID': client_id,
+            'Authorization': 'Bearer ' + keys['access_token']
+        }
+
+        print(headers)
+
+        stream = requests.get('https://api.twitch.tv/helix/streams?user_login=' + streamer_name, headers=headers)
+
+        stream_data = stream.json()
+
+        print(stream_data)
+
+        if len(stream_data['data']) == 1:
+            print(streamer_name + ' is live: ' + stream_data['data'][0]['title'] + ' playing ' + stream_data['data'][0]['game_name'])
+        else:
+            print(streamer_name + ' is not live')
+
+
 
 def setup(bot):
     bot.add_cog(id(bot))
