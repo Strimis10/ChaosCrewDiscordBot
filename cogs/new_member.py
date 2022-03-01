8fe6b8128c3161a0ffb77b4d27b35a6f4543606e
Strimis10 <Strimis10@gmail.com>
2022-03-01 11:21:58 +0100

    g

--- cogs/new_member.py
+++ cogs/new_member.py
@@ -1,7 +1,7 @@
-from discord.ext import commands
-import discord
-import discord.utils
-from discord.utils import get
+from nextcord.ext import commands
+import nextcord
+import nextcord.utils
+from nextcord.utils import get
 import json
 import os
 import time
@@ -16,8 +16,8 @@ class new(commands.Cog):
         self.bot = bot
     
 
-    @commands.Cog.listener()
-    async def on_ready(bot):
+    @commands.command(name="new")
+    async def new(ctx):
         while(True):
             from datetime import date
 
@@ -34,57 +34,51 @@ class new(commands.Cog):
             y = str(date.today())
             print(y)
             
-            
-            with open("date.json") as fj: 
-                feeds = json.load(fj)
-            print(feeds)
-            
-            if feeds != y:
-                print("not1")
-                with open("date.json", mode='w') as f:
-                    f.write(json.dumps(y, indent=2))
+        
+
+            print("old")
+            with open("new.json") as oj: 
+                o = json.load(oj)
+
+            for user in o:
+                if o[user] == 69.420:
+                    pass
+                elif o[user] >= 7:
+                    o[user] = 69.420
+                    # user1 = await self.bot.get_user_info(user)
+                    # username = user1.name
+                    client = nextcord.Client
+                    #username = client.get_user(user)
+                    print(user)
+                    print
+                    (type(user))
+                    #user2 = discord.Member(user)
+                    # user1 = user - user[0]
+                    # print(len(user1))
+                    #user2 = user1 - user[len(user1)]
+                    # 0user2 = user
+                    #nextcord.member.Member
+                    member = user
+                    #guild = ctx.bot.get_guild(int(786013884216639509))
+                    #guild = client.get_guild(786013884216639509)
+                    #member = guild.get_member(int(user))
+                    
+                    #role = discord.utils.get(member.guild.roles, id=946936153687347230)
+
+                    print(member)
+                    #role = ctx.guild.get_role(int(946936153687347230))
+                    #print(role)
+                    #await member.remove_roles(role)
 
 
-                print("old")
-                with open("new.json") as oj: 
-                    o = json.load(oj)
+                    # await user2.remove_roles(role)
 
-                for user in o:
-                    if o[user] == 69.420:
-                        pass
-                    elif o[user] >= 7:
-                        o[user] = 69.420
-                        # user1 = await self.bot.get_user_info(user)
-                        # username = user1.name
-                        client = discord.Client
-                        #username = client.get_user(user)
-                        print(user)
-                        print
-                        (type(user))
-                        #user2 = discord.Member(user)
-                        # user1 = user - user[0]
-                        # print(len(user1))
-                        #user2 = user1 - user[len(user1)]
-                        # 0user2 = user
+                else:
+                    o[user] = o[user] + 1
 
-                        #guild = client.get_guild(786013884216639509)
-                        guild = bot.get_guild(786013884216639509)
-                        member = guild.get_member(int(user))
-                        print(member)
-                        role = guild.get_role(int(946936153687347230))
-                        print(role)
-                        await member.remove_roles(role)
-
-
-                        # role = discord.utils.get(user2.guild.roles, id=946936153687347230)
-                        # await user2.remove_roles(role)
-
-                    else:
-                        o[user] = o[user] + 1
-
-                with open("new.json", mode='w') as f:
-                    f.write(json.dumps(o, indent=2))
-            await asyncio.sleep(10)
+            with open("new.json", mode='w') as f:
+                f.write(json.dumps(o, indent=2))
+        #await asyncio.sleep(10)
 
 
 
