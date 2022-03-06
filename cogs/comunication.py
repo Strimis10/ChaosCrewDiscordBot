66360a3624b1fc7ea88fa3e77aafbe4f716bf112
Strimis10 <Strimis10@gmail.com>
2022-03-06 13:58:10 +0100

    u

--- cogs/comunication.py
+++ cogs/comunication.py
@@ -1,10 +1,13 @@
+from http import client
 from discord.ext import commands
 import discord
 import discord.utils
+import asyncio
 
 class coms(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
 
     
     # CHAIR COLOR:
@@ -26,9 +29,30 @@ class coms(commands.Cog):
         
         #channel = await self.bot.fetch_channel(949590152202813453)
         #guild= discord.utils.get(self.bot.channels, id=int(949590152202813453))
-        await ctx.send("Request sent to ")
+        user = discord.utils.get(self.bot.get_all_members(), id=427822985102098434)
+        print(user)
+        print(user.status)
+        # print(type(user.status))
+        # print(type(user))
+
+        if str(user.status) == "offline":
+            print("what")
+            await ctx.send(f"{user.name} is {user.status}; request cannot be fulfilled.")
+        elif str(user.status) == "idle":
+            await ctx.send(f"{user.name} is {user.status}: request can but won't be fulfilled.")
+        elif str(user.status) == "dnd":
+            await ctx.send(f"{user.name} is in {user.status} mode (do not disturb): request can but won't be fulfilled.")
+        elif str(user.status) == "online":
+            await ctx.send("Request sent to DAHH")
+            await self.bot.get_channel(949590152202813453).send(f"69 {text.lower()}: requested by: {ctx.author} (:{ctx.author.id}:) channel.id :{ctx.channel.id}")
         
-        await self.bot.get_channel(949590152202813453).send(f"69:{text.lower()}: requested by: {ctx.author} :{ctx.author.id}")
+        
+        # try:
+        #     msg = await self.client.wait_for("message", check=check, timeout=30) # 30 seconds to reply
+        # except asyncio.TimeoutError:
+        #     await ctx.send("Sorry, you didn't reply in time!")
+       
+        
 
 
     
