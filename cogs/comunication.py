1826963e065bdd07d6fca3f515a5ba73b10b5421
Strimis10 <Strimis10@gmail.com>
2022-03-16 17:53:54 +0100

    gsf

--- cogs/comunication.py
+++ cogs/comunication.py
@@ -24,8 +24,41 @@ class coms(commands.Cog):
     #
     #
 
-    @commands.command(name='chair_color',description='',brief='')
-    async def say(self, ctx, *, text: commands.clean_content = ''):
+    # @commands.command(name='chair_color',description='',brief='')
+    # async def say(self, ctx, *, text: commands.clean_content = ''):
+        
+    #     #channel = await self.bot.fetch_channel(949590152202813453)
+    #     #guild= discord.utils.get(self.bot.channels, id=int(949590152202813453))
+    #     user = discord.utils.get(self.bot.get_all_members(), id=427822985102098434)
+    #     # print(user)
+    #     # print(user.status)
+    #     # print(type(user.status))
+    #     # print(type(user))
+
+    #     if str(user.status) == "offline":
+    #         print("what")
+    #         await ctx.send(f"{user.name} is {user.status}; request cannot be fulfilled.")
+    #     elif str(user.status) == "idle":
+    #         await ctx.send(f"{user.name} is {user.status}: request can but won't be fulfilled.")
+    #     elif str(user.status) == "dnd":
+    #         await ctx.send(f"{user.name} is in {user.status} mode (do not disturb): request can but won't be fulfilled.")
+    #     elif str(user.status) == "online":
+    #         await ctx.send("Request sent to DAHH")
+    #         await self.bot.get_channel(949590152202813453).send(f"69 {text.lower()}: requested by: {ctx.author} (:{ctx.author.id}:) channel.id :{ctx.channel.id}")
+        
+        
+    #     try:
+    #         msg = await self.client.wait_for("message", check=check, timeout=30) # 30 seconds to reply
+    #     except asyncio.TimeoutError:
+    #         await ctx.send("Sorry, you didn't reply in time!")
+       
+
+
+
+
+
+    @commands.command(name='Feed_Strimis',aliases=['FS','feed_strimis'],brief='Just think Snackaru with Skittles but worse')
+    async def Feed_Strimis (self, ctx):
         
         #channel = await self.bot.fetch_channel(949590152202813453)
         #guild= discord.utils.get(self.bot.channels, id=int(949590152202813453))
@@ -44,15 +77,18 @@ class coms(commands.Cog):
             await ctx.send(f"{user.name} is in {user.status} mode (do not disturb): request can but won't be fulfilled.")
         elif str(user.status) == "online":
             await ctx.send("Request sent to DAHH")
-            await self.bot.get_channel(949590152202813453).send(f"69 {text.lower()}: requested by: {ctx.author} (:{ctx.author.id}:) channel.id :{ctx.channel.id}")
+            await self.bot.get_channel(949590152202813453).send(f"69Feed_Strimis: requested by: {ctx.author} (:{ctx.author.id}:) channel.id :{ctx.channel.id}")
         
         
         # try:
         #     msg = await self.client.wait_for("message", check=check, timeout=30) # 30 seconds to reply
         # except asyncio.TimeoutError:
         #     await ctx.send("Sorry, you didn't reply in time!")
-       
-        
+
+
+
+    
+            
 
 
     
@@ -60,16 +96,47 @@ class coms(commands.Cog):
     async def on_message(self, message):
         if message.author.id != 932687176997687316:
         # import pdb
+            num = 0
+            ge = False
         # pdb.set_trace()
             error = ""
+            ga = ""
+            h = False
             for letter in message.content:
                 #print(letter)
                 if letter != " ":
-                    error = error + letter
+                    #print(letter)
+                    if letter != ":":
+                        error = error + letter
                 elif letter == " ":
+
                     break
+
+            for letter in message.content:
+                #print(letter)
+                if letter == ":":
+                    ge = True
+                elif letter != " ":
+                    if ge == True:
+                        ga = ga + letter
+                
+                elif letter == " ":
+                    num = num + 1
+                    if num == 2:
+                        break
+                # print(num)
+                # print(ge)
+                # print(ga)
+
+                
             if error == "Error:":
                 await self.bot.get_channel(932684556572700776).send(f"{message.content}")
+            elif ga == "Feed_Strimis":
+                #print("hug")
+                await self.bot.get_channel(int(error)).send(f"{message.content}")
+            elif ga == "Air_raid":
+                #print("hug")
+                await self.bot.get_channel(int(error)).send(f"{message.content}")
 
 
         
